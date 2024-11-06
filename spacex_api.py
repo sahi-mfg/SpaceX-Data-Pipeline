0acb2eea63d22d3258f5f3e5ae490d8b8bd78f71
Sahi Mohamed Francis Gonsangbeu <mohamedfrancissahi@gmail.com>
2024-11-06 21:05:57 +0100

    feat: :sparkles: enhance data output by displaying first few rows, columns, and data types from SpaceX API

--- spacex_api.py
+++ spacex_api.py
@@ -4,4 +4,9 @@ import pandas as pd
 url: str = "https://api.spacexdata.com/v4/rockets"
 
 data: pd.DataFrame = pd.read_json(url)
-print(data)
+
+
+print(data.head())
+print(data.columns)
+
+print(data.dtypes)
